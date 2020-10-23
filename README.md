3e6216ac599247dfa1fb336f6290c9497b515874
gfw-breaker <lixiaoming0513@gmail.com>
2020-10-23 17:04:32 +0800

    ok

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ---
 ##### 友情链接：[免翻墙看禁闻](https://github.com/gfw-breaker/banned-news3) &nbsp;|&nbsp; [翻墙软件下载](https://github.com/gfw-breaker/nogfw)
 
-| 视频节目名称 | 视频/音频 | 简体字幕 | 正體字幕 | 简体文稿 |
-|---|---|---|---|---|
+| 书名 | 下载 | 下载 | 下载 |
+|---|---|---|---|
+| 《九评共产党》 | [TXT](https://y2mate.com/zh-cn/search/) | [PDF](?raw=true) | [EPUB](?raw=true) |
+| 《解体党文化》 | [TXT](https://y2mate.com/zh-cn/search/) | [PDF](?raw=true) | [EPUB](?raw=true) |
+| 《共产主义的终极目的》 | [TXT](https://y2mate.com/zh-cn/search/) | [PDF](?raw=true) | [EPUB](?raw=true) |
--- publish.sh
+++ publish.sh
@@ -3,8 +3,6 @@
 
 git pull
 
-langs="zh"
-
 index=README.md
 
 ## README page
@@ -14,41 +12,16 @@ cat > $index << EOF
 ---
 ##### 友情链接：[免翻墙看禁闻](https://github.com/gfw-breaker/banned-news3) &nbsp;|&nbsp; [翻墙软件下载](https://github.com/gfw-breaker/nogfw)
 
-| 视频节目名称 | 视频/音频 | 简体字幕 | 正體字幕 | 简体文稿 |
-|---|---|---|---|---|
+| 书名 | 下载 | 下载 | 下载 |
+|---|---|---|---|
 EOF
 
 ## generate
-for lang in $langs; do
-	months=$(ls -r $lang)
-	for month in $months; do
-		dir="$lang/$month"
-		readme="$dir/readme.txt"
-		while read line; do
-			vid=$(echo $line | cut -d'.' -f1)
-			desc=$(echo $line | cut -d'.' -f2)
-			id=$(echo $line | cut -d'.' -f3)
-			srt="$dir/$vid.srt"
-			txt="$dir/$vid.txt"
-			srtTw="$dir/$vid.tw.srt"
-			txtTw="$dir/$vid.tw.txt"
-			cat $srt | grep -v ^$ | egrep -v ' --> [0-9]+:' | egrep -v ^[0-9]+$  > $txt
-			if [ -f $srt ]; then
-				opencc -c s2tw.json -i $srt -o $srtTw
-			else 
-				opencc -c tw2s.json -i $srtTw -o $srt
-			fi
-			#opencc -c s2tw.json -i $txt -o $txtTw
-			if [[ ! $vid =~ ^e ]]; then
-				txtLink=" [下载]($txt?raw=true) "
-			else 
-				txtLink=" [下载]($txt?raw=true) "
-				#txtLink="-"
-			fi
-			echo "| $desc | [下载](https://y2mate.com/zh-cn/search/$id) | [下载]($srt?raw=true) | [下載]($srtTw?raw=true) | $txtLink |" >> $index
-		done < $readme
-	done
-done
+while read line; do
+	key=$(echo $line | cut -d',' -f1)
+	name=$(echo $line | cut -d',' -f2)
+	echo "| 《$name》 | [TXT](https://y2mate.com/zh-cn/search/$id) | [PDF]($srt?raw=true) | [EPUB]($srtTw?raw=true) |" >> $index
+done < index.csv
 
 ## commit
 git add *
